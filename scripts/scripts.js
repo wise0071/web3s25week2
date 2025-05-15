36ef28f67c847388d8f528681b0fa451354315ea
qwiz <wise0071@algonquinlive.com>
2025-05-15 11:03:12 -0400

    added demo from wk2

---
+++ scripts/scripts.js
@@ -0,0 +1,27 @@
+//console.log will output a message in the dev console
+//this is a comment
+console.log("work");
+
+/*
+this is a multi line comment
+*/
+
+//to declare a variable use let or const
+const ourClassNum = "J206";
+let numStudents = 9; //no quotes needed for number
+
+console.log(ourClassNum);
+
+//if you declare a variable with let, you can change its value
+numStudents = 21;
+
+console.log(numStudents);
+
+//if you declare a value with const, you cant change its value
+//ourClassNum = "C128";
+//(if you are trying to change a const variable, you will get a error and will stop the code)
+console.log(ourClassNum);
+
+//const and let cannot be redeclared(recreated)
+//let numStudents = 25;
+//showing that we cannot recycle let/var with different number (already declared on line11)
